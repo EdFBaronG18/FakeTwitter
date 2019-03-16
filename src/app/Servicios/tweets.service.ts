1aead4135f4c1338d7be9b3e5ad7a9bcbd2976f1
Estudiante Politecnico Grancolombiano <general@poligran.edu.co>
2019-03-16 14:59:16 -0500

    Asincronía

---
+++ src/app/Servicios/tweets.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { ALL_TWEETS } from '../model/data';
+import { Tweet } from '../model/tweet';
+import { Observable , of} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class TweetsService {
+
+  constructor() { }
+
+  getAllTweets(): Observable<Tweet[]>{
+    return of(ALL_TWEETS);
+  }
+}
---
+++ src/app/model/data.ts
@@ -0,0 +1,24 @@
+import {Tweet} from "./tweet";
+
+export const ALL_TWEETS: Tweet[] =[{   
+    id: 1,
+    texto: 'You know something is unusual when your code runsperfect at first time',
+    autor: 'JustADevGuy'
+    },
+
+ {
+    id: 2,
+    texto: 'You know something is unusual when your code runsperfect at first time',
+    autor: 'EdFBaronG'
+    },
+{
+    id: 3,
+    texto: '1969: What are you doing with that 2KB of RAM? -sendingpeople to the moon',
+    autor: 'Alex_Gal'
+    },
+{
+    id: 4,
+    texto: '2019: What are you doing with that 1.5GB of RAM? -Justchecking facebook',
+    autor: 'GDAndres98'
+
+}];
